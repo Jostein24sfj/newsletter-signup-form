2819334216b6e36055da4162508a2a64b211f4ba
Jostein24sfj <jostein24sfj@gmail.com>
2025-02-17 09:36:55 +0100

    Second commit. Style ble endret når javascript ble lagt til.

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -5,17 +5,44 @@ const card = document.getElementById("card");
 console.log(card);
 
 const btn = document.getElementById("btn");
+const modalBtn = document.getElementById("modalBtn");
+
+const emailInput = document.querySelector(".email input");
+console.log(emailInput);
+
+// function isValidEmail(email) {
+//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+// }
 
 btn.onclick = function () {};
+const emailValue = emailInput.value.trim();
 
-window.onclick = function (event) {
-  if (event.target == card) {
-    card.style.display = "none";
-  }
-};
+if (!isValidEmail(emailValue)) {
+  alert("Please enter a valid email adress.");
+}
 
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
+btn.addEventListener("click"),
+  function () {
+    const emailValue = emailInput.value.trim();
+
+    btn.onclick = function (event) {
+      // if (event.target == card) {
+      card.style.display = "none";
+      modal.style.display = "block";
+      // }
+    };
+
+    modalBtn.onclick = function (event) {
+      // if (event.target == card) {
+      card.style.display = "flex";
+      modal.style.display = "none";
+      emailInput.value = "";
+      // }
+    };
+
+    // window.onclick = function (event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // };
+  };
